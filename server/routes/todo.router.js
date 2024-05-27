fd45fdb2c988fe43c1eff8db497ca7d0f159428b
Kalia Xiong <kaliaxyooj@gmail.com>
2024-05-27 12:41:57 -0500

    added post router

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -21,6 +21,28 @@ router.get('/', function (request, response) {
 });
 
 // POST
+router.post('/', function (request, response) {
+    console.log('request.body', request.body);
+
+    let newToDo = request.body;
+
+    let toDo = newToDo.todo;
+    let complete = newToDo.complete;
+   
+    let queryText = `INSERT INTO "todolist" ("toDo", "complete")
+    VALUES ($1, $2);`
+    pool.query(queryText, [toDo, complete])
+        .then(dbResult => {
+            console.log('dbResult.rows', dbResult.rows);
+            response.sendStatus(201);
+        })
+        .catch(dbError => {
+            console.log('dbError:', dbError);
+            response.sendStatus(500);
+
+            console.log('Adding treats', newTreat);
+        })
+});
 
 // PUT
 
