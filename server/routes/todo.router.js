5baa7a81113c350bf477e1454e0c0215f6746f78
Kalia Xiong <kaliaxyooj@gmail.com>
2024-05-27 12:06:26 -0500

    get router added.

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -3,6 +3,22 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // GET
+router.get('/', function (request, response) {
+
+    let queryText = 'SELECT * FROM "todolist";';
+
+    pool.query(queryText)
+        .then((dbResult) => {
+            let toDoList = dbResult.rows;
+            console.log('get dbResult:', dbResult);
+            response.send(toDoList);
+        })
+        .catch((dbError) => {
+            console.log('dbError:', dbError);
+            response.sendStatus(500);
+        })
+
+});
 
 // POST
 
